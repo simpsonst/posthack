3b89cd70d443b743624b82811cb6df8dcd3fc1da
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2023-04-18 15:50:39 +0100

    Licence fix.

--- src/python/apps/config.py
+++ src/python/apps/config.py
@@ -1,4 +1,4 @@
-## Copyright (c) 2022, Lancaster University
+## Copyright (c) 2023, Lancaster University
 ## All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
